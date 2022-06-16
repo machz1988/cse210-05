c6c8e9eb4138bf16a6a5847a62a8e093c15adc20
Antonio-Saucedo <antoniojesus@byui.edu>
2022-06-16 09:03:11 -0600

    Work with Manuel, Godwin and Cole

---
+++ cycle/game/scripting/control_cycle2_action.py
@@ -0,0 +1,48 @@
+import constants
+from game.scripting.control_cycle1_action import ControlCycle1Action
+from game.shared.point import Point
+
+class ControlCycle2Action(ControlCycle1Action):
+    """
+    An input action that controls the cycles.
+
+    The responsibility of ControlActorsAction is to get the direction and move the cycles.
+
+    Attributes:
+        _keyboard_service (KeyboardService): An instance of KeyboardService.
+    """
+
+    def __init__(self, keyboard_service):
+        """Constructs a new ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+        self._keyboard_service = keyboard_service
+        self._direction = Point(constants.CELL_SIZE, 0)
+
+    def execute(self, cast, script):
+        """Executes the control actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+        
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+        
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+        
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction = Point(0, constants.CELL_SIZE)
+        
+        cycle2 = cast.get_first_actor("cycle2")
+        cycle2.turn_head(self._direction)
---
+++ cycle/game/scripting/handle_collisions_action.py
@@ -0,0 +1,76 @@
+import constants
+from game.casting.actor import Actor
+from game.scripting.action import Action
+from game.shared.point import Point
+
+class HandleCollisionsAction(Action):
+    """
+    An update action that handles interactions between the actors.
+    
+    The responsibility of HandleCollisionsAction is to handle the situation when the cycles collides
+    with the each other, or the cycle collides with its segments, or the game is over.
+
+    Attributes:
+        _is_game_over (boolean): Whether or not the game is over.
+    """
+
+    def __init__(self):
+        """Constructs a new HandleCollisionsAction."""
+        self._is_game_over = False
+
+    def execute(self, cast, script):
+        """Executes the handle collisions action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        if not self._is_game_over:
+            self._handle_segment_collision(cast)
+            self._handle_game_over(cast)
+    
+    def _handle_segment_collision(self, cast):
+        """Sets the game over flag if the cycle collides with one of its segments.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        cycle1 = cast.get_first_actor("cycle1")
+        head1 = cycle1.get_segments()[0]
+        segments1 = cycle1.get_segments()[1:]
+        cycle2 = cast.get_first_actor("cycle2")
+        head2 = cycle2.get_segments()[0]
+        segments2 = cycle2.get_segments()[1:]
+        
+        for segment1 in segments1:
+            if head1.get_position().equals(segment1.get_position()):
+                self._is_game_over = True
+        for segment2 in segments2:
+            if head2.get_position().equals(segment2.get_position()):
+                self._is_game_over = True
+
+    def _handle_game_over(self, cast):
+        """Shows the 'game over' message and turns the cycles white if the game is over.
+        
+        Args:
+            cast (Cast): The cast of Actors in the game.
+        """
+        if self._is_game_over:
+            cycle1 = cast.get_first_actor("cycle1")
+            segments1 = cycle1.get_segments()
+            cycle2 = cast.get_first_actor("cycle2")
+            segments2 = cycle2.get_segments()
+
+            x = int(constants.MAX_X / 2)
+            y = int(constants.MAX_Y / 2)
+            position = Point(x, y)
+
+            message = Actor()
+            message.set_text("Game Over!")
+            message.set_position(position)
+            cast.add_actor("messages", message)
+
+            for segment1 in segments1:
+                segment1.set_color(constants.WHITE)
+            for segment2 in segments2:
+                segment2.set_color(constants.WHITE)
